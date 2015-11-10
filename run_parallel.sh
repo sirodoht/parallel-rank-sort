4d51e0a63ae5a8b76c254f948bf7c3bfdb3b88ba
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-11-10 15:10:05 +0200

    add true for random argument on run script

--- run_parallel.sh
+++ run_parallel.sh
@@ -7,6 +7,6 @@ do
 	for i in 1 2 4 8 16 24 32 46 64;
 	do
 		echo ${j}x${i}
-		java ParallelRankSort ${j} ${i} > results/${j}x${i}.txt
+		java ParallelRankSort ${j} ${i} true > results/${j}x${i}.txt
 	done
 done;
