75b871f96ec2ae5ae5efbc3ff0d5583892688c17
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-11-17 23:16:58 +0200

    add semicolons on bash commands

--- run_parallel.sh
+++ run_parallel.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir results
-javac -Xdiags:verbose ParallelRankSort.java
+mkdir results;
+javac -Xdiags:verbose ParallelRankSort.java;
 for j in 1000 10000 100000;
 do
 	for i in 1 2 4 8 16 24 32 46 64;
 	do
-		echo ${j}x${i}
-		java ParallelRankSort ${j} ${i} true > results/${j}x${i}.txt
-	done
+		echo ${j}x${i};
+		java ParallelRankSort ${j} ${i} true > results/${j}x${i}.txt;
+	done;
 done;
